009e14ff20b62550947b50e4a9f6b77fc4594602
Karthick <pkarthicbz@live.in>
2023-04-18 10:56:40 +0530

    replace comma with dot on last skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -32,6 +32,8 @@ export default function Skills() {
       [...items].forEach(item=>{
         setPara(para += item.skill+', ');
       });
+      // para[para.length-2] = para[para.length-2].replace(',', '');
+      setPara(para = para.slice(0, para.length-2)+'.');
       e.target.parentNode.classList.add('hide-skills-editor');
       e.target.parentNode.parentNode.childNodes[0].classList.remove('skill--para');
     }else{
